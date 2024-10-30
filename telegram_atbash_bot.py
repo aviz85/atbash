f73019e6f139b715368994ada5c6f2e53f1a4333
Aviz85 <avizmaeir@gmail.com>
2024-10-31 00:11:00 +0200

    initilize app

--- telegram_atbash_bot.py
+++ telegram_atbash_bot.py
@@ -38,7 +38,9 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 if not TOKEN:
     raise ValueError("No token found! Make sure to set TELEGRAM_BOT_TOKEN in .env file")
 
+# Initialize application properly
 application = Application.builder().token(TOKEN).build()
+application.initialize()
 
 # Add secret token as constant
 WEBHOOK_SECRET_TOKEN = "your_secret_token"  # Should match the one in set_webhook.py
